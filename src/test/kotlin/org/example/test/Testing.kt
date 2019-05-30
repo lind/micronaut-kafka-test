99c65d5a63adf897ef073107b1b42b9c7ae213ab
Jonas Lindholm <jdlindholm@gmail.com>
2019-05-30 22:39:40 +0200

    Added Kotlin

---
+++ src/test/kotlin/org/example/test/Testing.kt
@@ -0,0 +1,13 @@
+package org.example.test
+
+import org.junit.jupiter.api.Test
+
+class Testing {
+
+    @Test
+    fun tesing() {
+        println("******************************************")
+        println("Yes!!!")
+        println("******************************************")
+    }
+}
